9f7eeba3a49f149c3237b9f2bffdb2b629751565
xlanor <contact@jingk.ai>
2019-11-04 17:40:26 +0800

    Added decorator w/o syntactic sugar

--- Theory/Decorators/decorator_demo.py
+++ Theory/Decorators/decorator_demo.py
@@ -15,5 +15,19 @@ def random_fn(arg1):
         raise Exception
     print("No exception :)")
 
+# With syntactic sugar
 random_fn(True)
 random_fn(False)
+
+def random_fn_2(arg1):
+    print("Inner function ran")
+    if arg1:
+        raise Exception
+    print("No exception :)")
+
+# Now, rewrite it without syntactic sugar.
+print("==================")
+catch_all(random_fn_2)(True)
+catch_all(random_fn_2)(False)
+
+# I kind of blew this - I panicked too much during the interview.
